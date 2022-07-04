bc545e2a8a1e3480addda9103d521aeb4df3cb5d
LauraMontalvo <93949016+LauraMontalvo@users.noreply.github.com>
2022-07-04 08:38:36 -0500

    cambios del proyecto LS

---
+++ lightswitch/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LightSwitch from './Componentes/LightSwitch';
+
+function App() {
+  return (
+    <div className="App">
+      <LightSwitch/>
+      
+    </div>
+  );
+}
+
+export default App;
